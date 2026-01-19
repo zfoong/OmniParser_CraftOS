5dd307ea0d1ba3d3a89e5835c9e3000fdb2cf43f
ahmad-ajmal <ahmadajmal1514@gmail.com>
2026-01-19 09:24:54 +0500

    PaddleOCR args remove

--- util/utils.py
+++ util/utils.py
@@ -22,12 +22,12 @@ from paddleocr import PaddleOCR
 reader = easyocr.Reader(['en'])
 paddle_ocr = PaddleOCR(
     lang='en',  # other lang also available
-    use_angle_cls=False,
-    use_gpu=False,  # using cuda will conflict with pytorch in the same process
-    show_log=False,
-    max_batch_size=1024,
-    use_dilation=True,  # improves accuracy
-    det_db_score_mode='slow',  # improves accuracy
+    # use_angle_cls=False,
+    # use_gpu=False,  # using cuda will conflict with pytorch in the same process
+    # show_log=False,
+    # max_batch_size=1024,
+    # use_dilation=True,  # improves accuracy
+    # det_db_score_mode='slow',  # improves accuracy
     rec_batch_num=1024)
 import time
 import base64
